059d2b980845a687f79c4db16515e02831386962
deepayan.sarkar <deepayan.sarkar@4f709643-ed21-0410-99af-65da78b75472>
2007-01-09 03:03:02 +0000

    added comments describing how to replace ESS's completion rather than just define an alternative

--- trunk/rcompgen/inst/examples/altesscomp.el
+++ trunk/rcompgen/inst/examples/altesscomp.el
@@ -1,11 +1,27 @@
 
-;;; A proof-of-concept alternative user completion command (adapted
-;;; from ess-complete-object-name) for use with ESS, making use of the
-;;; rcompgen package rather than ESS's built-in completion mechanism.
-;;; Including this in your .emacs file should be sufficient (see usage
-;;; note below).  The R package 'rcompgen' must be installed.
+;;; This is a proof-of-concept alternative user completion command
+;;; (adapted from ess-complete-object-name) for use with ESS, making
+;;; use of the rcompgen package rather than ESS's built-in completion
+;;; mechanism.  Including this in your .emacs file should be
+;;; sufficient (see usage note below).  The R package 'rcompgen' must
+;;; be installed.
 
 (require 'ess-site)
+
+;;; There are two ways to proceed.
+
+;;; (1) You can completely replace 'ess-complete-object-name', in
+;;; which case you should keep the following function definition, but
+;;; change the name 'alt-ess-complete-object-name' to
+;;; 'ess-complete-object-name'.  The advantage to this approach is
+;;; that you retain all the other ESS goodies like filename
+;;; completion, C-c C-TAB completion in code buffers, etc.  The
+;;; downside is that you won't be able to use the original
+;;; 'ess-complete-object-name' any more.
+
+;;; (2) Define the new function with a different name, and use
+;;; whichever one you want (this is what will happen if you use this
+;;; file as-is):
 
 (defun alt-ess-complete-object-name ()
 
@@ -26,17 +42,11 @@
 	    (ess-get-words-from-vector "rcompgen:::.retrieveCompletions()\n"))))
     (or (comint-dynamic-simple-complete token-string possible-completions) 'none)))
 
-;; This completion function can now be invoked as 
-;; M-x alt-ess-complete-object-name
+;;; This completion function can now be invoked as 
+;;; M-x alt-ess-complete-object-name
 
-;; However, it is more common to associate completion with the TAB
-;; key, which can be done as follows:
+;;; If you want to associate completion with the TAB key, that can be
+;;; done as follows:
 
-(define-key inferior-ess-mode-map "\t" 'alt-ess-complete-object-name)
-
-;; Unlike ESS, this does not revert to filename completion when inside
-;; quotes (mostly because I haven't figured out how to do that).  You
-;; can still do filename completion with M-TAB (note that many window
-;; managers will catch Alt-TAB for window navigation, so you might
-;; need to use Esc-TAB for this).
+;; (define-key inferior-ess-mode-map "\t" 'alt-ess-complete-object-name)
 
